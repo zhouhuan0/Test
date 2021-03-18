4c6c19425c309129e067a5cbb5a16456febf1629
zhouhuan <zhouhuan@youkeshu.com>
2021-03-18 14:20:57 +0800

    Initial commit

---
+++ src/main/java/com/yks/App.java
@@ -0,0 +1,13 @@
+package com.yks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
